82c5395d24e0a15cd1bb6022578763ae49958c6d
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-01-11 18:12:42 +0200

    responsive+small changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 (function () {
 
-    $(".navt li a").on("click", function () {
-        console.log('hi');
-        $(".navt").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });
+  
 
 })(jQuery);
 
