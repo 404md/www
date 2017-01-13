5a6ffa648d39e29680ab2c07362c52b306545d73
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-01-13 19:08:54 +0200

    members and half contact page without responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+(function () {
+    
+      $('.our-members').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dot:false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+
+})(jQuery);
 
 function NavigationFunction() {
     var x = document.getElementById("myTopnav");
@@ -6,4 +20,13 @@ function NavigationFunction() {
     } else {
         x.className = "navt";
     }
+}
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.2),
+        zoom: 15
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
 }
