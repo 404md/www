3d72e5d0c9eba212c5bdc3bfa0e959c39c55d957
tkaciuk <itcaciuc@pycoding.biz>
2017-01-23 18:56:32 +0200

    #404 Changes svg files buttons percentage (#12)
    * Changes svg files buttons percentage
    
    * hot fix

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function myMap() {
     var mapCanvas = document.getElementById("map");
     var mapOptions = {
         center: new google.maps.LatLng(46.9861808, 28.8577161),
-        zoom: 18,
+        zoom: 18
 
     }
     var map = new google.maps.Map(mapCanvas, mapOptions);
