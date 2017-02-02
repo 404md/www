ee490685fe4c91f225f426c0145a5d7710dd33b6
Eugene <eistrati@mitocgroup.com>
2017-02-02 14:09:36 -0500

    adding ro and ru versions / skeleton

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ uglifyjs ./js/members.js -c -m -o ./js/members.min.js
 echo "Copying resources into build/release folder"
 rm -rf ./build/Release/*
 mkdir -p ./build/Release/
-cp -R favicon.png robots.txt sitemap.xml css fonts images js amenities contact members pricing privacy terms ./build/Release/
+cp -R favicon.png robots.txt sitemap.xml css fonts images js amenities contact members pricing terms ro ru ./build/Release/
 
 echo "Minifying html code"
 html-minifier --collapse-whitespace --remove-comments ./index.html -o ./build/Release/index.html
@@ -24,8 +24,21 @@ html-minifier --collapse-whitespace --remove-comments ./amenities/index.html -o 
 html-minifier --collapse-whitespace --remove-comments ./contact/index.html -o ./build/Release/contact/index.html
 html-minifier --collapse-whitespace --remove-comments ./members/index.html -o ./build/Release/members/index.html
 html-minifier --collapse-whitespace --remove-comments ./pricing/index.html -o ./build/Release/pricing/index.html
-html-minifier --collapse-whitespace --remove-comments ./privacy/index.html -o ./build/Release/privacy/index.html
 html-minifier --collapse-whitespace --remove-comments ./terms/index.html -o ./build/Release/terms/index.html
+
+html-minifier --collapse-whitespace --remove-comments ./ro/index.html -o ./build/Release/ro/index.html
+html-minifier --collapse-whitespace --remove-comments ./ro/amenities/index.html -o ./build/Release/ro/amenities/index.html
+html-minifier --collapse-whitespace --remove-comments ./ro/contact/index.html -o ./build/Release/ro/contact/index.html
+html-minifier --collapse-whitespace --remove-comments ./ro/members/index.html -o ./build/Release/ro/members/index.html
+html-minifier --collapse-whitespace --remove-comments ./ro/pricing/index.html -o ./build/Release/ro/pricing/index.html
+html-minifier --collapse-whitespace --remove-comments ./ro/terms/index.html -o ./build/Release/ro/terms/index.html
+
+html-minifier --collapse-whitespace --remove-comments ./ru/index.html -o ./build/Release/ru/index.html
+html-minifier --collapse-whitespace --remove-comments ./ru/amenities/index.html -o ./build/Release/ru/amenities/index.html
+html-minifier --collapse-whitespace --remove-comments ./ru/contact/index.html -o ./build/Release/ru/contact/index.html
+html-minifier --collapse-whitespace --remove-comments ./ru/members/index.html -o ./build/Release/ru/members/index.html
+html-minifier --collapse-whitespace --remove-comments ./ru/pricing/index.html -o ./build/Release/ru/pricing/index.html
+html-minifier --collapse-whitespace --remove-comments ./ru/terms/index.html -o ./build/Release/ru/terms/index.html
 
 echo "Synchronizing build/Release/"
 aws s3 sync --delete build/Release/ s3://www.404.md/ --region eu-central-1 --storage-class REDUCED_REDUNDANCY --metadata-directive REPLACE --cache-control max-age=604800
