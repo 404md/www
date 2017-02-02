39b0cffc7272d6b60edff1707d8e330405423c5d
tkaciuk <itcaciuc@pycoding.biz>
2017-02-02 19:37:03 +0200

    Footer-gallery new style (#21)

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@
 #npm install cssnano-cli -g
 #npm install uglify-js -g
 
-echo "Minifying css code"
-cssnano < ./css/index.css > ./css/index.min.css
+# we use --compressed flag so we do not need to compress it again
+#echo "Minifying css code"
+#cssnano < ./css/index.css > ./css/index.min.css
 
 echo "Minifying js code"
 uglifyjs ./js/main.js -c -m -o ./js/main.min.js
