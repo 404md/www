2c8a82748aa9025fc786e55e9c343d13f48115d1
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-02-22 10:28:19 +0200

    ru ro

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 (function($) {
-    window.fnames = new Array();
-    window.ftypes = new Array();
+    window.fnames = [];
+    window.ftypes = [];
     fnames[0]='EMAIL';ftypes[0]='email';
     fnames[1]='FNAME';ftypes[1]='text';
     // fnames[2]='LNAME';ftypes[2]='text';
