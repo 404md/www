87e9d5fbd8a932178805e8146e068d490acee893
tkaciuk <itcaciuc@pycoding.biz>
2017-02-22 22:57:58 +0200

    #404-md Book a tour ru ro en+responsive (#34)
    * Tour page _Half Invitation page
    
    * hot fix
    
    * respoonsive booking tour
    
    * ru ro
    
    * Book a tour ru ru en+rensponsive

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 (function($) {
-    window.fnames = new Array();
-    window.ftypes = new Array();
+    window.fnames = [];
+    window.ftypes = [];
     fnames[0]='EMAIL';ftypes[0]='email';
     fnames[1]='FNAME';ftypes[1]='text';
     // fnames[2]='LNAME';ftypes[2]='text';
@@ -8,6 +8,7 @@
     fnames[4]='MMERGE4';ftypes[4]='text';
     fnames[7]='MMERGE7';ftypes[7]='date';
     fnames[5]='MMERGE5';ftypes[5]='text';
+    fnames[6]='MMERGE6';ftypes[6]='radio';
 
     $.extend($.validator.messages, {
         required: "This field is required.",
