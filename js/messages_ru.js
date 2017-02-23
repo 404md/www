39097ab89ef884f3d0d8a2808f21ccf8847ae6b6
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-02-23 18:28:56 +0200

    Error messages in Tour page + responsive for pricing

---
+++ js/messages_ru.js
@@ -0,0 +1,37 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: RU (Russian; русский язык)
+ */
+
+(function($) {
+    window.fnames = [];
+    window.ftypes = [];
+    fnames[0]='EMAIL';ftypes[0]='email';
+    fnames[1]='FNAME';ftypes[1]='text';
+    // fnames[2]='LNAME';ftypes[2]='text';
+    fnames[3]='MMERGE3';ftypes[3]='phone';
+    fnames[4]='MMERGE4';ftypes[4]='text';
+    fnames[7]='MMERGE7';ftypes[7]='date';
+    fnames[5]='MMERGE5';ftypes[5]='text';
+    fnames[6]='MMERGE6';ftypes[6]='radio';
+    
+$.extend( $.validator.messages, {
+    required: "Это поле необходимо заполнить.",
+    remote: "Пожалуйста, введите правильное значение.",
+    email: "Пожалуйста, введите корректный адрес электронной почты.",
+    url: "Пожалуйста, введите корректный URL.",
+    date: "Пожалуйста, введите корректную дату.",
+    dateISO: "Пожалуйста, введите корректную дату в формате ISO.",
+    number: "Пожалуйста, введите число.",
+    digits: "Пожалуйста, вводите только цифры.",
+    creditcard: "Пожалуйста, введите правильный номер кредитной карты.",
+    equalTo: "Пожалуйста, введите такое же значение ещё раз.",
+    extension: "Пожалуйста, выберите файл с правильным расширением.",
+    maxlength: $.validator.format( "Пожалуйста, введите не больше {0} символов." ),
+    minlength: $.validator.format( "Пожалуйста, введите не меньше {0} символов." ),
+    rangelength: $.validator.format( "Пожалуйста, введите значение длиной от {0} до {1} символов." ),
+    range: $.validator.format( "Пожалуйста, введите число от {0} до {1}." ),
+    max: $.validator.format( "Пожалуйста, введите число, меньшее или равное {0}." ),
+    min: $.validator.format( "Пожалуйста, введите число, большее или равное {0}." )
+} );
+}(jQuery));
