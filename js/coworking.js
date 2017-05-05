66fefc11f8045712dd6160d48e6c1d39881e31cb
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-05-05 18:40:11 +0300

    New footer

---
+++ js/coworking.js
@@ -0,0 +1,29 @@
+jQuery(function($) {
+    "use strict";
+
+    // $('.offers').slick({
+    //     infinite: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     dots: false,
+    //     responsive: [
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 3,
+    //                 infinite: true,
+    //                 dots: false
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //             }
+    //         }
+    //     ]
+    // });
+
+});
