055495814938a73ebb12723adc5434d55e69dcc7
Irina Tkaciuk <tkaciukirinaa@gmail.com>
2017-08-16 15:37:50 +0300

    Magic-line, popup effects, parallax

--- js/main.js
+++ js/main.js
@@ -43,25 +43,25 @@ jQuery(function($) {
     $('#menu-toggle').click(function () {
         $(this).toggleClass('open');
         $('.main-nav').toggleClass("responsive-nav");
-        if ( $('#menu-toggle').hasClass('open')) {
-            $('html').css('overflow','hidden');
+        if ($('#menu-toggle').hasClass('open')) {
+            $('html').css('overflow', 'hidden');
         }
         else {
-            $('html').css('overflow','scroll');
+            $('html').css('overflow', 'scroll');
         }
     });
 
     var isScrolling;
     var navigationBar = $('.navt a');
 
-    navigationBar.on('touchstart', function() {
+    navigationBar.on('touchstart', function () {
         isScrolling = false;
     });
-    navigationBar.on('touchmove', function(e) {
+    navigationBar.on('touchmove', function (e) {
         isScrolling = true;
     });
-    navigationBar.on('touchend', function(e) {
-        if(!isScrolling) {
+    navigationBar.on('touchend', function (e) {
+        if (!isScrolling) {
             window.location = $(this).attr('href');
         }
     });
@@ -71,7 +71,7 @@ jQuery(function($) {
     var $addButton = $('.add-circle');
     var count = 0;
 
-    function  hideAndShowTrash() {
+    function hideAndShowTrash() {
         // destination.find('.clone-div').each(function(i, el) {
         //     if(count == 1) {
         //         $($(el).find('.trash')[0]).hide();
@@ -85,7 +85,7 @@ jQuery(function($) {
 
         console.log(itemCount);
 
-        $trashes.each(function() {
+        $trashes.each(function () {
             if (itemCount > 1) {
                 $(this).show();
             } else {
@@ -129,7 +129,7 @@ jQuery(function($) {
 
         hideAndShowTrash();
 
-        $('.mail-input').on('change', function() {
+        $('.mail-input').on('change', function () {
             var $parent = $(this).closest('.clone-div');
 
             $parent.find('.change-color').toggleClass('get-sales');
@@ -140,7 +140,7 @@ jQuery(function($) {
 
     $addButton.trigger('click');
 
-    destination.on('click', '.trash', function() {
+    destination.on('click', '.trash', function () {
         if (count > 0) {
             $(this).closest('.clone-div').remove();
             $('.total').removeClass('gray-circle');
@@ -150,87 +150,87 @@ jQuery(function($) {
         hideAndShowTrash();
         refreshTotalPercentage();
     });
-    
-    $('.button-play').on('click', function(){
+
+    $('.button-play').on('click', function () {
         $('.image').addClass('video');
         $('.media-image').hide();
         $('.show-video').removeClass('hidden');
         $('#video').attr('src', 'https://www.youtube.com/embed/ZGbORDi_UPA?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1');
     });
 
-    $('.btn-close').on('click', function(){
+    $('.btn-close').on('click', function () {
         $('.show-video').addClass('hidden');
         $('.image').removeClass('video');
         $('.media-image').fadeIn("slow");
         $('#video').removeAttr('src');
     });
 
-    var popupOverlay =  $('.overlay');
+    var popupOverlay = $('.overlay');
     var popupOverlay1 = $('.overlay1');
     var popUp = $(".popup");
 
-    $('.getprice-button').on('click',function(){
+    $('.getprice-button').on('click', function () {
         popupOverlay.addClass('pop-up-visible');
-        $('html').css('overflow','hidden');
+        $('html').css('overflow', 'hidden');
         popUp.show();
     });
 
     var BtnClose = $('.close');
-    BtnClose.on('click',function(){
+    BtnClose.on('click', function () {
         popupOverlay.removeClass('pop-up-visible');
-        $('html').css('overflow','scroll');
+        $('html').css('overflow', 'scroll');
     });
 
-    $('.getprice-button1').on('click',function(){
+    $('.getprice-button1').on('click', function () {
         popupOverlay1.addClass('pop-up-visible');
-        $('html').css('overflow','hidden');
+        $('html').css('overflow', 'hidden');
         popUp.show();
     });
 
-    $(document).on('click', '.pop-up-visible', function(e){
-        if (!($(e.target).hasClass( "popup" ))) {
+    $(document).on('click', '.pop-up-visible', function (e) {
+        if (!($(e.target).hasClass("popup"))) {
             popUp.hide("fast");
             popupOverlay1.removeClass('pop-up-visible');
             popupOverlay.removeClass('pop-up-visible');
-            $('html').css('overflow','scroll');
+            $('html').css('overflow', 'scroll');
         }
 
     });
 
-    BtnClose.on('click',function(){
+    BtnClose.on('click', function () {
         popupOverlay1.removeClass('pop-up-visible');
     });
 
-    $(':required, .required').on('blur keydown', function() {
-        $(this)[ $(this).val() ? 'addClass' : 'removeClass' ]('touched');
+    $(':required, .required').on('blur keydown', function () {
+        $(this)[$(this).val() ? 'addClass' : 'removeClass']('touched');
     });
-    
-        $(".icons").hover(function(){
-            $(this).addClass('text-color');
-            $(this).parent().prev().addClass('text-color');
-        }, function(){
-            $(this).parent().prev().removeClass('text-color');
-            $(this).removeClass('text-color');
-        });
+
+    $(".icons").hover(function () {
+        $(this).addClass('text-color');
+        $(this).parent().prev().addClass('text-color');
+    }, function () {
+        $(this).parent().prev().removeClass('text-color');
+        $(this).removeClass('text-color');
+    });
 
 
-        $(".color-link").hover(function(){
-            $(this).addClass('text-color');
-            $(this).next().find('.icons').addClass('text-color');
-        }, function(){
-            $(this).next().find('.icons').removeClass('text-color');
-            $(this).removeClass('text-color');
-        });
+    $(".color-link").hover(function () {
+        $(this).addClass('text-color');
+        $(this).next().find('.icons').addClass('text-color');
+    }, function () {
+        $(this).next().find('.icons').removeClass('text-color');
+        $(this).removeClass('text-color');
+    });
 
 
     $(window).scroll(function () {
-            if ($(window).scrollTop() > 10) {
-                $('.main-nav').addClass('fixed-top');
-            }
-            else {
-                $('.main-nav').removeClass("fixed-top");
-            }
-        });
+        if ($(window).scrollTop() > 10) {
+            $('.main-nav').addClass('fixed-top');
+        }
+        else {
+            $('.main-nav').removeClass("fixed-top");
+        }
+    });
 
 
     $(function () {
@@ -243,7 +243,7 @@ jQuery(function($) {
                 var output = '';
                 $.each(response.items, function (k, item) {
                     var visibleSm;
-                    if(k < 2){
+                    if (k < 2) {
                         visibleSm = '';
                     } else {
                         visibleSm = ' visible-sm';
@@ -253,11 +253,11 @@ jQuery(function($) {
                     var tagIndex = item.description.indexOf('<img');
                     var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex;
                     var srcStart = srcIndex + 5;
-                    var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; 
-                    var src = item.description.substring(srcStart, srcEnd); 
-                    output += '<a href="'+ item.link + '" class="blog-element"><img class="img-responsive" src="' + src + '" height="208px" ></a></header>';
-                    output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
-                    var yourString = item.description.replace(/<img[^>]*>/g,""); 
+                    var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart;
+                    var src = item.description.substring(srcStart, srcEnd);
+                    output += '<a href="' + item.link + '" class="blog-element"><img class="img-responsive" src="' + src + '" height="208px" ></a></header>';
+                    output += '<div class="blog-content"><h4><a href="' + item.link + '">' + item.title + '</a></h4>';
+                    var yourString = item.description.replace(/<img[^>]*>/g, "");
                     var maxLength = 120;
                     output += '</div></div></div>';
                     return k < 2;
@@ -266,5 +266,73 @@ jQuery(function($) {
             }
         });
     });
-});
+    $('.open-popup').on('click', function () {
+        $('.popup-open-1').addClass('md-show');
+    });
 
+    $('.md-trigger').on('click', function () {
+        $('html').css('overflow', 'hidden');
+    });
+    $('.mdi-close').on('click', function () {
+        $('html').css('overflow', 'scroll');
+    });
+
+    function scrollBanner() {
+        $(document).scroll(function(){
+            var scrollPos = $(this).scrollTop();
+            $('.scroll-title').css({
+                'top' : (scrollPos/2.8)+'px'
+            });
+
+        });
+    }
+    scrollBanner();
+
+    var j$ = jQuery,
+        $nav = j$("#navigation"),
+        $slideLine = j$("#slide-line"),
+        $currentItem = j$(".current-item");
+
+    j$(function(){
+        // Menu has active item
+        if ($currentItem[0]) {
+            $slideLine.css({
+                "width": $currentItem.width(),
+                "left": $currentItem.position().left
+            });
+        }
+
+        // Underline transition
+        j$($nav).find("li").hover(
+            // Hover on
+            function(){
+                $slideLine.css({
+                    "width": j$(this).width(),
+                    "left": j$(this).position().left
+                });
+            },
+            // Hover out
+            function(){
+                if ($currentItem[0]) {
+                    // Go back to current
+                    $slideLine.css({
+                        "width": $currentItem.width(),
+                        "left": $currentItem.position().left
+                    });
+                } else {
+                    // Disapear
+                    $slideLine.width(0);
+                }
+                if($(window).width() < 1246) {
+                    $slideLine.width(0);
+                } else {
+                    $slideLine.css({
+                        "width": $currentItem.width(),
+                        "left": $currentItem.position().left
+                    });
+                }
+            }
+        );
+    });
+   
+});
