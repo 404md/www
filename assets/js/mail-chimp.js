2e380374a30c1c6d05e5ff51d9544b37d7ef84f4
ddimitrioglo <ddimitrioglo@pycoding.biz>
2018-03-06 18:05:26 +0200

    #404MD fix mailChimp initializing

--- assets/js/mail-chimp.js
+++ assets/js/mail-chimp.js
@@ -8,7 +8,7 @@ $(function() {
      */
     $('#subscribe-form').MailChimpForm({
         url: '//mitocgroup.us11.list-manage.com/subscribe/post?u=13a7a5fca813b378c24ec9fe3&id=092d77b13b',
-        fields: '1:NAME,PHONE,0:EMAIL,2:MESSAGE',
+        fields: '1:NAME,4:PHONE,0:EMAIL,2:MESSAGE',
         submitSelector: '#submit-form'
     });
 
