c6945db033973e7dfc8daec097daca3f23ed8909
Valkora <vcorostinscaea@gmail.com>
2018-03-26 16:13:39 +0300

    Unused imgs and code parts are removed and theres some changes at forms js file

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -5,6 +5,27 @@ jQuery(function($) {
   const $tourForm = $('#subscribe-tour-form');
   const $contactForm = $('#subscribe-contact-form');
 
+  $('#mce-PHONE').on('input', function() {
+    let input=$(this);
+    let re = /^[\+]?[(]?[0-9]{3}[)]?[0-9]*$/;
+    let is_phone=re.test(input.val());
+    let $genErr = $('#phone-error');
+
+    if(is_phone){
+      let errMsg = "";
+      $genErr.html(`<p>${errMsg}</p>`);
+      $('#submit-form').prop('disabled',false);
+    }
+    else{
+      let errMsg = "Unacceptable value";
+
+      $genErr.html(`<p>${errMsg}</p>`);
+      $('#submit-form').prop('disabled',true);
+    }
+
+  });
+
+
   if ($contactForm.length) {
     /**
      * Contact form
@@ -13,14 +34,13 @@ jQuery(function($) {
       url: '//mitocgroup.us11.list-manage.com/subscribe/post?u=13a7a5fca813b378c24ec9fe3&id=092d77b13b',
       fields: '1:NAME,4:PHONE,0:EMAIL,2:MESSAGE',
       submitSelector: '#submit-form',
-        onFail: function (errMsg) {
-            let $genErr = $('#mc-general-error');
+      onFail: function (errMsg) {
+        let $genErr = $('#mc-general-error');
 
-            $genErr.html(`<div class="error-mc">${errMsg}</div>`);
-            //setTimeout(() => { $genErr.html(''); }, 5000);
-        },
+        $genErr.html(`<div class="error-mc">${errMsg}</div>`);
+      },
       onOk: function(okMsg) {
-          window.location='/thank-you'
+        window.location='/thank-you'
       }
     });
   }
@@ -33,34 +53,34 @@ jQuery(function($) {
       url: '//mitocgroup.us11.list-manage.com/subscribe/post?u=13a7a5fca813b378c24ec9fe3&id=de06a08172',
       fields: '0:EMAIL,1:FNAME,2:LNAME,3:PHONE,4:JOB,5:VDATE,6:MMERGE3,7:LANG',
       submitSelector: '#submit-form',
-        onFail: function (errMsg) {
-            let $genErr = $('#mc-general-error');
+      onFail: function (errMsg) {
+        let $genErr = $('#mc-general-error');
 
-            let lnid = $( "input:checked" ).attr("id");
-            if(lnid==="eng_btn"){
+        let lnid = $( "input:checked" ).attr("id");
+        if (lnid === "engBtn" ){
 
-            }
-            if(lnid==="rom_btn"){
-               errMsg="Nu se poate asa";
-            }
-            if(lnid==="ru_btn"){
-                errMsg="Что-то пошло не так, какая жалость"
-            }
-
-            $genErr.html(`<div class="error-mc">${errMsg}</div>`);
-        },
-      onOk: function(okMsg) {
-           let lnid = $( "input:checked" ).attr("id");
-           if(lnid==="eng_btn"){
-               window.location='/thank-you'
-           }
-            if(lnid==="rom_btn"){
-                window.location='/thank-you'
-            }
-            if(lnid==="ru_btn"){
-                window.location='/thank-you'
-            }
         }
+        if(lnid==="rom_btn"){
+          errMsg="Nu se poate asa";
+        }
+        if(lnid==="ru_btn"){
+          errMsg="Что-то пошло не так, какая жалость"
+        }
+
+        $genErr.html(`<div class="error-mc">${errMsg}</div>`);
+      },
+      onOk: function(okMsg) {
+        let lnid = $( "input:checked" ).attr("id");
+        if(lnid==="eng_btn"){
+          window.location='/thank-you'
+        }
+        if(lnid==="rom_btn"){
+          window.location='/thank-you'
+        }
+        if(lnid==="ru_btn"){
+          window.location='/thank-you'
+        }
+      }
     });
   }
 
