0bbcb144b685dd4d164d9089cace97a8678024df
KanoniPan <panevschi.ruslan@gmail.com>
2018-03-29 19:00:42 +0300

    Updated README.MD and added deploy-lambdas script

--- bin/deploy-lambda.sh
+++ bin/deploy-lambda.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 isNpmPackageInstalled() {
-  npm list --depth 1 -g $1 > /dev/null 2>&1
+  which node-lambda $1 > /dev/null 2>&1
 }
 
 for package in node-lambda
@@ -17,22 +17,22 @@ LAMBDA=$([ -n "$1" ] && echo "$1" || echo 'this_lambda_doesnt_exist')
 PROFILE=$([ -n "$2" ] && echo "$2" || echo 'default')
 
 if [ ${LAMBDA} = 'facebook-events-404md' ]; then
-  cd backend/FacebookEvents
-  npm install
-  node-lambda deploy
-  aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
+    cd backend/FacebookEvents
+    npm install
+    node-lambda deploy
+    aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
 elif [ ${LAMBDA} = 'medium-feed-404md' ]; then
-  cd backend/MediumFeed
-  npm install
-  node-lambda deploy
-  aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
+    cd backend/MediumFeed
+    npm install
+    node-lambda deploy
+    aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
 elif [ ${LAMBDA} = 'instagram-feed-404md' ]; then
-  cd backend/InstagramFeed
-  npm install
-  node-lambda deploy
-  aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
+    cd backend/InstagramFeed
+    npm install
+    node-lambda deploy
+    aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
 else
-  echo 'please enter valid lambda facebook-events-404md || medium-feed-404md || instagram-feed-404md'
+    echo 'please enter valid lambda facebook-events-404md || medium-feed-404md || instagram-feed-404md'
 fi
 
 done
