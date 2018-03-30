278ddb52aa8aef41622baf065643a2299de6598f
ddimitrioglo <ddimitrioglo@pycoding.biz>
2018-03-31 01:02:39 +0300

    #404MD cleanup code

--- bin/deploy-lambda.sh
+++ bin/deploy-lambda.sh
@@ -7,20 +7,25 @@ fi
 
 LAMBDA=$([ -n "$1" ] && echo "$1" || echo 'this_lambda_doesnt_exist')
 PROFILE=$([ -n "$2" ] && echo "$2" || echo 'default')
+AVAILABLE=('medium-feed-404md' 'instagram-feed-404md' 'facebook-events-404md')
+
+if [[ ! " ${AVAILABLE[@]} " =~ "${LAMBDA}" ]]; then
+    echo "Please enter valid lambda name: facebook-events-404md || medium-feed-404md || instagram-feed-404md"
+fi
 
 if [ ${LAMBDA} = 'facebook-events-404md' ]; then
     cd backend/FacebookEvents
-    node-lambda deploy
-    aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
 elif [ ${LAMBDA} = 'medium-feed-404md' ]; then
     cd backend/MediumFeed
-    npm install
-    node-lambda deploy
-    aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
 elif [ ${LAMBDA} = 'instagram-feed-404md' ]; then
     cd backend/InstagramFeed
-    node-lambda deploy
-    aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
-else
-    echo 'Please enter valid lambda facebook-events-404md || medium-feed-404md || instagram-feed-404md'
 fi
+
+echo "Installing lambda's dependencies..."
+npm install
+
+echo "Start deploying lambda..."
+node-lambda deploy
+
+echo "Invoking lambda function..."
+aws lambda invoke --function-name ${LAMBDA} /dev/null --profile ${PROFILE}
